147c6e90c6817cff2ccde9f77ee9d178ff59938d
lwisniewski <lwisniewski@tellmeplus.com>
2016-10-12 13:25:52 +0200

    log added

--- Main.py
+++ Main.py
@@ -6,6 +6,16 @@ import datetime
 import time
 import sys
 from math import fabs
+import logging
+
+# Log system
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
+steam_handler = logging.StreamHandler()
+steam_handler.setLevel(logging.DEBUG)
+steam_handler.setFormatter(formatter)
+log.addHandler(steam_handler)
 
 updater = Updater(token='281531409:AAF513XUt-FB_jv9eIxd0SSImg-BbMLeXkw')
 dispatcher = updater.dispatcher
@@ -16,11 +26,15 @@ cal = Calendar.from_ical(file.read())
 
 
 def refresh_cal():
+    global log
+    log.info("Refreshing calendar.")
     global cal
     cal = Calendar.from_ical(file.read())
 
 
 def give_room(bot, update):
+    global log
+    log.info("Give the room")
     global lastUpdate
     global cal
     if datetime.datetime.utcnow() - timedelta(hours=2) > lastUpdate:
@@ -36,7 +50,7 @@ def give_room(bot, update):
         if t1 < t2 and fabs(t2 - t1) < min_diff:
             min_diff = t2 - t1
             mine = e
-    bot.sendMessage(chat_id=update.message.chat_id, text=mine["LOCATION"])
+    bot.sendMessage(chat_id=update.message.chat_id, text=(mine["SUMMARY"] + "\n" + mine["LOCATION"]))
 
 
 start_handler = CommandHandler('room', give_room)

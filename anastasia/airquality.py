7be35975c4e25b28769ca42ee0d809212924f2bf
DUPONT <francoisdupont05@gmail.com>
2017-01-17 18:40:16 +0100

    update /airquality

--- anastasia/airquality.py
+++ anastasia/airquality.py
@@ -3,51 +3,53 @@
 import urllib.request
 import json
 
+# Adapt the google map bounds to get more stations within the air pollution API
+# This value works fine for Grenoble
+const_bounds = 0.05
+unavailable_stations = 0
+nb_stations = 0
+grenoble = "Grenoble"
 
-def get_indice_from_url(url):
+
+def get_indice_from_bounds(url):
+    """Return the average aqi of all stations founded within the area."""
+    global nb_stations
+    global unavailable_stations
+
     tmp = urllib.request.urlopen(url).read().decode('utf8')
     j = json.loads(tmp)
-    return int(j["data"]["aqi"])
-
-
-def get_indice_grenoble():
+    nb_stations= len(j["data"])
     indice = 0
-    # 5 air quality stations in Grenoble
-    # based Latitude/Longitude ==> GRAND BOULEVARD
-    url = "http://api.waqi.info/feed/geo:45.1666700;5.7166700/?token=f092408be447fbe8a44f9849baf81f46acfc704f"
-    indice += get_indice_from_url(url)
-    # based Latitude/Longitude ==> SMH
-    url = "http://api.waqi.info/feed/geo:45.1766700;5.7566700/?token=f092408be447fbe8a44f9849baf81f46acfc704f"
-    indice += get_indice_from_url(url)
-    # based Latitude/Longitude ==> ROCADE SUD
-    url = "http://api.waqi.info/feed/geo:45.1566700;5.7166700/?token=f092408be447fbe8a44f9849baf81f46acfc704f"
-    indice += get_indice_from_url(url)
-    # based Latitude/Longitude ==> LES FRENES
-    url = "http://api.waqi.info/feed/geo:45.1566700;5.7266700/?token=f092408be447fbe8a44f9849baf81f46acfc704f"
-    indice += get_indice_from_url(url)
-    # based Latitude/Longitude ==> FONTAINE
-    url = "http://api.waqi.info/feed/geo:45.1766700;5.6866700/?token=f092408be447fbe8a44f9849baf81f46acfc704f"
-    indice += get_indice_from_url(url)
-    indice = int(indice / 5)
+    for i in range(0,nb_stations):
+        #station off
+        if j["data"][i]["aqi"] == "-":
+            unavailable_stations = unavailable_stations + 1
+            continue;
+        indice+=int(j["data"][i]["aqi"])
+    diff = nb_stations-unavailable_stations
+    if diff != 0:
+        indice = int(indice / diff)
     return indice
 
 
 def get_json_google_maps(city):
+    """Return json from the request of Google Geocode API"""
     url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + city + "&Key=AIzaSyAxdCmDbyuyzQ5IZmVhlYrF2fCLnRmDKhI"
     tmp = urllib.request.urlopen(url).read().decode('utf8')
     return json.loads(tmp)
 
-def get_latitude_longitute(j):
-    res = str(j["results"][0]["geometry"]["location"]["lat"]) + ";"
-    res += str(j["results"][0]["geometry"]["location"]["lng"])
+
+def get_bounds(j):
+    """Return the bounds where the air stations can be found through the air pollution API"""
+    res = str(j["results"][0]["geometry"]["bounds"]["southwest"]["lat"]-const_bounds) + ","
+    res += str(j["results"][0]["geometry"]["bounds"]["southwest"]["lng"]-const_bounds) + ","
+    res += str(j["results"][0]["geometry"]["bounds"]["northeast"]["lat"]+const_bounds) + ","
+    res += str(j["results"][0]["geometry"]["bounds"]["northeast"]["lng"]+const_bounds)
     return res
 
 
-def get_formatted_address(j):
-    return j["results"][0]["formatted_address"]
-
-
 def format_args(args):
+    """Format the arguments for the google API"""
     res = ""
     for i in range(0, len(args)):
         res += "+"+str(args[i])
@@ -55,32 +57,32 @@ def format_args(args):
 
 
 def get_airquality(args):
-    if len(args) == 0:
-        indice = get_indice_grenoble()
-        airquality = "Qualité de l'air, Grenoble\n"
+    if len(args) > 0:
+        formated_args = format_args(args)
     else:
-        if len(args) > 1:
-            formated_args = format_args(args)
-        else:
-            formated_args = args[0]
-        j = get_json_google_maps(formated_args)
-        url = "http://api.waqi.info/feed/geo:"+get_latitude_longitute(j)+"/?token=f092408be447fbe8a44f9849baf81f46acfc704f"
-        indice = get_indice_from_url(url)
-        airquality = "Qualité de l'air, " + get_formatted_address(j) + "\n"
+        formated_args = grenoble
+    j = get_json_google_maps(formated_args)
+    url = "http://api.waqi.info/map/bounds/?latlng="+get_bounds(j)+"&token=f092408be447fbe8a44f9849baf81f46acfc704f"
+    indice = get_indice_from_bounds(url)
+    airquality = "Qualité de l'air, " + j["results"][0]["formatted_address"]
 
-    airquality += "Indice : " + str(indice) + " - "
-    if 0 < indice < 51:
-        airquality += "Bon"
-    elif 50 < indice < 101:
-        airquality += "Modéré"
-    elif 100 < indice < 151:
-        airquality += "Malsain pour les gens sensibles"
-    elif 150 < indice < 201:
-        airquality += "Malsain"
-    elif 200 < indice < 251:
-        airquality += "Vraiment malsain"
-    elif 250 < indice:
-        airquality += "Dangereux pour la santé"
+    if nb_stations-unavailable_stations > 0:
+        airquality += "\nIndice : " + str(indice) + " - "
+        if 0 < indice < 51:
+            airquality += "Bon"
+        elif 50 < indice < 101:
+            airquality += "Modéré"
+        elif 100 < indice < 151:
+            airquality += "Malsain pour les gens sensibles"
+        elif 150 < indice < 201:
+            airquality += "Malsain"
+        elif 200 < indice < 251:
+            airquality += "Vraiment malsain"
+        elif 250 < indice:
+            airquality += "Dangereux pour la santé"
+
+    if unavailable_stations > 0:
+        airquality += "\nStations disponibles : " + str(nb_stations-unavailable_stations) + "/" + str(nb_stations)
 
     return airquality
 

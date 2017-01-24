b57b1b7f61e9c0f51b7d64d1aa5fe8874be46a8e
lwisniewski <lwisniewski@tellmeplus.com>
2017-01-24 10:49:06 +0100

    /nude is for women too

--- anastasia/nude.py
+++ anastasia/nude.py
@@ -1,11 +1,20 @@
 import urllib.request
+import random
 from bs4 import BeautifulSoup
 
 
 def get_nude(bot, update):
-    site = urllib.request.urlopen("http://dites.bonjourmadame.fr/random")
-    html = site.read().decode('iso-8859-1')
-    soup = BeautifulSoup(html, 'html.parser')
+    if random.random() < 0.5:
+        site = urllib.request.urlopen("http://dites.bonjourmadame.fr/random")
+        html = site.read().decode('iso-8859-1')
+        soup = BeautifulSoup(html, 'html.parser')
 
-    nude = soup.find("div", attrs={"class": "photo post"})
-    bot.sendPhoto(chat_id=update.message.chat_id, photo=nude.a.img['src'])
+        nude = soup.find("div", attrs={"class": "photo post"})
+        bot.sendPhoto(chat_id=update.message.chat_id, photo=nude.a.img['src'])
+    else:
+        site = urllib.request.urlopen("http://www.bonjourmonsieur.fr/monsieur/random.html")
+        html = site.read().decode('iso-8859-1')
+        soup = BeautifulSoup(html, 'html.parser')
+
+        nude = soup.find("div", attrs={"class": "img"})
+        bot.sendPhoto(chat_id=update.message.chat_id, photo="http://www.bonjourmonsieur.fr/" + nude.h1.img['src'])

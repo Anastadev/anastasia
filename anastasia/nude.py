1e47c2ebfccd375a3c567c8822458a35b9d6d134
Dan <dan.devoc@gmail.com>
2017-01-26 09:18:29 +0100

    Final version of nude v1.1

--- anastasia/nude.py
+++ anastasia/nude.py
@@ -16,7 +16,7 @@ class Nude:
 
     def get_nude(self,bot, update, args):
         if len(args) > 0 and args[0] == "men":
-            self.increase(T_MEN, update.message.from_user.id)
+            self.increase(T_MEN, update.message.from_user.id,update.message.from_user.username)
             site = urllib.request.urlopen("http://www.bonjourmonsieur.fr/monsieur/random.html")
             html = site.read().decode('iso-8859-1')
             soup = BeautifulSoup(html, 'html.parser')
@@ -24,7 +24,7 @@ class Nude:
             nude = soup.find("div", attrs={"class": "img"})
             bot.sendPhoto(chat_id=update.message.chat_id, photo="http://www.bonjourmonsieur.fr/" + nude.h1.img['src'])
         else:
-            self.increase(T_WOMEN,update.message.from_user.id)
+            self.increase(T_WOMEN,update.message.from_user.id,update.message.from_user.username)
             site = urllib.request.urlopen("http://dites.bonjourmadame.fr/random")
             html = site.read().decode('iso-8859-1')
             soup = BeautifulSoup(html, 'html.parser')
@@ -32,11 +32,14 @@ class Nude:
             nude = soup.find("div", attrs={"class": "photo post"})
             bot.sendPhoto(chat_id=update.message.chat_id, photo=nude.a.img['src'])
 
-    def increase(self,type_,userId_):
-        cursor = self.db.find({"userId":userId_})
+    def increase(self,type_,userId_,userName_):
+        cursor = self.db.find({"user.id":userId_})
         if cursor.count()==0:
             doc = {
-                "userId":userId_,
+                "user":{
+                    "id":userId_,
+                    "username":userName_,
+                },
                 "lastUsage":int(time.time()),
                 "count":{
                     "daily":1,
@@ -49,12 +52,13 @@ class Nude:
             self.db.insert_one(doc)
         else:
             doc = cursor[0]
+            doc["user"]["username"]=userName_
             currentTime = int(time.time())
-            if (currentTime-currentTime%86400)!=(doc["lastUsage"]-doc["lastUsage"]%86400):
+            if (currentTime-(currentTime%86400))!=(doc["lastUsage"]-(doc["lastUsage"]%86400)):
                 doc["count"]["daily"] = 1
             else:
                 doc["count"]["daily"] += 1
             doc["lastUsage"] = currentTime
             doc["count"][type_] += 1
-            self.db.replace_one({"userId":userId_},doc)
+            self.db.replace_one({"user.id":userId_},doc)
             cursor.close()

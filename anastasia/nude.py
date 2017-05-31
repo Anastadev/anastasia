8687531d6cc356a94386c942e3b5fa0051115773
metaheavens <lpw.wisniewki@gmail.com>
2017-05-31 13:59:56 +0200

    Nude men repaired

--- anastasia/nude.py
+++ anastasia/nude.py
@@ -15,12 +15,12 @@ class Nude:
 
     def get_nude(self,bot, update, args):
         if len(args) > 0 and args[0] == "men":
-            site = urllib.request.urlopen("http://www.bonjourmonsieur.fr/monsieur/random.html")
+            site = urllib.request.urlopen("http://www.pausephp.com/monsieur/random.html")
             html = site.read().decode('iso-8859-1')
             soup = BeautifulSoup(html, 'html.parser')
 
             nude = soup.find("div", attrs={"class": "img"})
-            bot.sendPhoto(chat_id=update.message.chat_id, photo="http://www.bonjourmonsieur.fr/" + nude.h1.img['src'])
+            bot.sendPhoto(chat_id=update.message.chat_id, photo="http://www.pausephp.com/" + nude.h1.img['src'])
             self.increase(T_MEN, update.message.from_user.id, update.message.from_user.username)
         else:
             site = urllib.request.urlopen("http://dites.bonjourmadame.fr/random")
